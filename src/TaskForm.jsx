0963c4d8873034b2eabb4050465c45e4f3c119e2
Angie Cohen <angiedev2001@gmail.com>
2023-03-09 18:57:30 -0300

    primer commit del proyecto con react

---
+++ src/TaskForm.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function TaskForm({ createTask }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createTask({
+        title,
+        description,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Escribe tu tarea"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        placeholder="Escribe la descripcion de tu tarea aqui"
+        onChange={(e) => setDescription(e.target.value)}
+      ></textarea>
+      <button>Guardar</button>
+    </form>
+  );
+}
+
+export default TaskForm;
